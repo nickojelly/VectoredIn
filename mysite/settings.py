bde449643bfa45f711d99c287bc111038b191d90
nickojelly <nickbarlow@live.com.au>
2024-06-20 11:31:08 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
-    'google_analytics',
+    # 'google_analytics',
 ]
 
 MIDDLEWARE = [
