24cbd47400c399e2ca571f443376d43539fe47a1
nickojelly <nickbarlow@live.com.au>
2024-07-19 00:12:00 +0100

    Updated plots

--- main/apps.py
+++ main/apps.py
@@ -1,15 +1,21 @@
 from django.apps import AppConfig
-from .utils import hnsw_intialize, initialize_openai_client, initialize_df, initialize_weaviate_client
-
+from django.utils.functional import lazy
+import pandas as pd
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
 
     def ready(self):
-        from .models import QueryEmbedding
-        # Call the initialization function from utils
-        initialize_df()
-        hnsw_intialize()
-        initialize_openai_client()
-        initialize_weaviate_client()
+        from .utils import (
+            hnsw_initialize, 
+            initialize_openai_client, 
+            initialize_df, 
+            initialize_weaviate_client
+        )
         
+        # Use lazy loading for initializations
+        self.get_df = lazy(initialize_df, pd.DataFrame)
+        self.get_hnsw = lazy(hnsw_initialize, object)
+        self.get_openai_client = lazy(initialize_openai_client, object)
+        self.get_weaviate_client = lazy(initialize_weaviate_client, object)
+
